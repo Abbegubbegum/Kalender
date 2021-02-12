9b92f6ae1c1a9db9023beaf8fcdcf767e7f3944d
Albin <albin.nojback@gmail.com>
2021-02-12 09:36:01 +0100

    Add javascript base

---
+++ js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let todoOutput = document.querySelector("[data-todo-list]");
+let todoForm = document.querySelector("[data-todo-form]");
+let todoInput = document.querySelector("[data-todo-input]");
+let todoList = ["Äta", "Sova", "Dansa"];
