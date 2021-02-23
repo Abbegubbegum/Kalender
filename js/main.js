709521f8f97c9c45f08296e34cb605ecdad72e26
Albin <albin.nojback@gmail.com>
2021-02-23 13:21:17 +0100

    Basic javascript done

--- js/main.js
+++ js/main.js
@@ -5,76 +5,39 @@ let todoForm = document.querySelector("[data-todo-form]");
 let todoInput = document.querySelector("[data-todo-input]");
 let todos = ["Äta", "Sova", "Dansa"];
 
-
-// todoForm.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     let input = todoInput.value.trim();
-
-//     if (input === "")
-//     {
-//         return;
-//     }
-//     todoList.push(input);
-//     update();
-//     todoInput.value = "";
-// });
-
-// function createList(items) {
-//     let list = document.createElement("ul");
-//     items.forEach((item) => {
-//         let listItem = document.createElement("li");
-//         listItem.innerText = item;
-        
-//         listItem.addEventListener("click", remove());
-//         list.append(listItem);
-//     })
-//     return list;
-// }
-
-// function remove(event) {
-//     let item = event.target.innerText;
-//     todoList = todoList.filter((e) => e !== item);
-//     update();
-// }
-
-// function update() {
-//     todoOutput.innerHTML = "";
-//     todoOutput.append(createList(todoList));
-// }
-
-// update();
-
 todoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (todoInput.value.trim() === "") {
-      return;
-    }
-    todos.push(todoInput.value.trim());
-    updateList();
-    todoInput.value = "";
+  e.preventDefault();
+  let input = todoInput.value.trim();
+
+  if (input === "") {
+    return;
+  }
+  todos.push(input);
+  update();
+  todoInput.value = "";
+});
+
+function createList(items) {
+  let list = document.createElement("ul");
+  items.forEach((item) => {
+    let listItem = document.createElement("li");
+    listItem.innerText = item;
+
+    listItem.addEventListener("click", remove);
+    list.append(listItem);
   });
-  
-  function todoList(items) {
-    let list = document.createElement("ul");
-    items.forEach((item) => {
-      let todoListItem = document.createElement("li");
-      todoListItem.innerText = item;
-      todoListItem.classList.add("todo-list-item");
-      todoListItem.addEventListener("click", removeItem);
-      list.append(todoListItem);
-    });
-    return list;
-  }
-  
-  function removeItem(event) {
-    let itemToRemove = event.target.innerText;
-    todos = todos.filter((item) => item !== itemToRemove);
-    updateList();
-  }
-  
-  function updateList() {
-    listOutput.innerHTML = "";
-    listOutput.append(todoList(todos));
-  }
-  
-  updateList();
+  return list;
+}
+
+function remove(event) {
+  let item = event.target.innerText;
+  todos = todos.filter((e) => e !== item);
+  update();
+}
+
+function update() {
+  todoOutput.innerHTML = "";
+  todoOutput.append(createList(todos));
+}
+
+update();
